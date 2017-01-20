12a684add033488a8d6e96946cb37c991f828c90
Joel de León <joel.dl08@gmail.com>
2017-01-20 03:35:17 -0600

    First version 1.0

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 
 const electron = require('electron');
 const app = electron.app;
+const Menu = electron.Menu;
 const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
 
+let willQuitApp = false;
 let mainWindow;
 
 app.on('window-all-closed', function() {
@@ -12,11 +15,34 @@ app.on('window-all-closed', function() {
   }
 });
 
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('https://web.whatsapp.com/');
+app.on('activate', function(){
+    mainWindow.show();
+});
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
+app.on('before-quit', function(){
+    willQuitApp = true;
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow(
+    {
+      width: 650,
+      height: 300,
+      titleBarStyle: 'hidden-inset',
+      // icon: path.join(__dirname, 'assets/icons/mac/todoist.icns')
+    }
+  );
+  mainWindow.loadURL('https://deleonn.github.io/ticketroll_deadline/');
+
+  mainWindow.on('close', function(e) {
+    if (willQuitApp) {
+     /* the user tried to quit the app */
+     mainWindow = null;
+   } else {
+     /* the user only tried to close the window */
+     e.preventDefault();
+     mainWindow.hide();
+   }
   });
+
 });
